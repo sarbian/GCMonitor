7019daf772c4a11666a3f817955ddfe913daf636
Sarbian <Sarbian@sarbian.com>
2015-06-18 00:01:39 +0200

    Use the Virtual Memory usage instead of the RSS on Windows

--- GCMonitor.cs
+++ GCMonitor.cs
@@ -26,10 +26,11 @@ using System;
 using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using UnityEngine;
 using System.Threading;
 using KSP.IO;
+using UnityEngine;
 using Debug = UnityEngine.Debug;
+using Resources = GCMonitor.Properties.Resources;
 
 namespace GCMonitor
 {
@@ -65,7 +66,7 @@ namespace GCMonitor
         int lastDisplayedSecond = 0;
 
         bool fullUpdate = true;
-        
+
         [Persistent]
         bool OnlyUpdateWhenDisplayed = true;
         [Persistent]
@@ -137,17 +138,21 @@ namespace GCMonitor
 
         // Windows x86
         private const string kDllPath_Win_x86 = "GameData/GCMonitor/getRSS_x86.dll";
-        [DllImport(dllName: kDllPath_Win_x86, EntryPoint = "getCurrentRSS")]
-        private static extern UIntPtr getCurrentRSS_Win_x86();
-        [DllImport(dllName: kDllPath_Win_x86, EntryPoint = "getPeakRSS")]
-        private static extern UIntPtr getPeakRSS_Win_x86();
+        [DllImport(dllName: kDllPath_Win_x86, EntryPoint = "getCurrentVM")]
+        private static extern UIntPtr getCurrentVM_Win_x86();
+        [DllImport(dllName: kDllPath_Win_x86, EntryPoint = "getMaximumVM")]
+        private static extern UIntPtr getMaximumVM_Win_x86();
 
-        // Windows x64 (not implemented yet)
+        // Windows x64
         private const string kDllPath_Win_x64 = "GameData/GCMonitor/getRSS_x64.dll";
-        [DllImport(dllName: kDllPath_Win_x64, EntryPoint = "getCurrentRSS")]
-        private static extern UIntPtr getCurrentRSS_Win_x64();
-        [DllImport(dllName: kDllPath_Win_x64, EntryPoint = "getPeakRSS")]
-        private static extern UIntPtr getPeakRSS_Win_x64();
+        [DllImport(dllName: kDllPath_Win_x64, EntryPoint = "getCurrentVM")]
+        private static extern UIntPtr getCurrentVM_Win_x64();
+        [DllImport(dllName: kDllPath_Win_x64, EntryPoint = "getMaximumVM")]
+        private static extern UIntPtr getMaximumVM_Win_x64();
+
+
+        // Current Linux and OSX still use the RSS. I'll have th check if it is the 
+        // right value for those (pretty sure it is not for Linux)
 
         // Linux x86
         private const string kDllPath_linux_x86 = "GameData/GCMonitor/getRSS_x86.so";
@@ -176,11 +181,11 @@ namespace GCMonitor
             return (UIntPtr)0;
         }
 
-        public delegate UIntPtr GetCurrentRSS();
-        public static GetCurrentRSS getCurrentRSS;
-        public delegate UIntPtr GetPeakRSS();
-        public static GetPeakRSS getPeakRSS;
-        
+        public delegate UIntPtr GetCurrentVM();
+        public static GetCurrentVM getCurrentVM;
+        public delegate UIntPtr GetMaximumVM();
+        public static GetMaximumVM getMaximumVM;
+
         public static long warnMem;
         public static long alertMem;
         public static string memoryString;
@@ -197,13 +202,7 @@ namespace GCMonitor
             }
 
             fpsPos.Set(fpsX, fpsY, 200, 50);
-
-            // I know. Should be ulong. User with EB of memory can file a bug
-            long maxAllowedMem = IsX64() ? long.MaxValue : uint.MaxValue;
-
-            warnMem = (long)(maxAllowedMem * warnPercent);
-            alertMem = (long)(maxAllowedMem * alertPercent);
-
+            
             updateRate = updateInterval;
 
             line = new Color[height];
@@ -212,7 +211,7 @@ namespace GCMonitor
                 blackLine[i] = Color.black;
 
             cat = new Texture2D(33, 20, TextureFormat.ARGB32, false);
-            cat.LoadImage(Properties.Resources.cat);
+            cat.LoadImage(Resources.cat);
             catPixels = cat.GetPixels();
             blackSquare = new Color[32 * height];
             for (int i = 0; i < blackSquare.Length; i++)
@@ -222,54 +221,67 @@ namespace GCMonitor
             spaceFormat.NumberGroupSeparator = " ";
 
             Debug.Log("[GCMonitor] Setting up getRSS delegates");
+            Debug.Log(Environment.CurrentDirectory.ToString());
             switch (Application.platform)
             {
                 case RuntimePlatform.LinuxPlayer:
                     if (IsX64())
                     {
-                        getCurrentRSS = getCurrentRSS_Linux_x64;
-                        getPeakRSS = getPeakRSS_Linux_x64;
+                        getCurrentVM = getCurrentRSS_Linux_x64;
+                        getMaximumVM = getPeakRSS_Linux_x64;
                     }
                     else
                     {
-                        getCurrentRSS = getCurrentRSS_Linux_x86;
-                        getPeakRSS = getPeakRSS_Linux_x86;
+                        getCurrentVM = getCurrentRSS_Linux_x86;
+                        getMaximumVM = getPeakRSS_Linux_x86;
                     }
                     break;
                 case RuntimePlatform.OSXPlayer:
-                    getCurrentRSS = getCurrentRSS_OSX_x86;
-                    getPeakRSS = getPeakRSS_OSX_x86;
+                    getCurrentVM = getCurrentRSS_OSX_x86;
+                    getMaximumVM = getPeakRSS_OSX_x86;
                     break;
                 case RuntimePlatform.WindowsPlayer:
                     if (IsX64())
                     {
-                        getCurrentRSS = getCurrentRSS_Win_x64;
-                        getPeakRSS = getPeakRSS_Win_x64;
+                        getCurrentVM = getCurrentVM_Win_x64;
+                        getMaximumVM = getMaximumVM_Win_x64;
                     }
                     else
                     {
-                        getCurrentRSS = getCurrentRSS_Win_x86;
-                        getPeakRSS = getPeakRSS_Win_x86;
+                        getCurrentVM = getCurrentVM_Win_x86;
+                        getMaximumVM = getMaximumVM_Win_x86;
                     }
                     break;
                 default:
-                    getCurrentRSS = unimplemented;
-                    getPeakRSS = unimplemented;
+                    getCurrentVM = unimplemented;
+                    getMaximumVM = unimplemented;
                     break;
             }
 
             try
             {
-                UIntPtr c = getCurrentRSS();
-                UIntPtr p = getPeakRSS();
-                Debug.Log("[GCMonitor] Delegates OK " + ConvertToKBString((long)c) + " / " + ConvertToKBString((long)p));
+                UIntPtr c = getCurrentVM();
+                UIntPtr p = getMaximumVM();
+                Debug.Log("[GCMonitor] Delegates OK " + ConvertToMBString((long)c) + " / " + ConvertToMBString((long)p));
             }
             catch (Exception e)
             {
                 Debug.Log("[GCMonitor] Unable to find getRSS implementation\n" + e.ToString());
-                getCurrentRSS = unimplemented;
-                getPeakRSS = unimplemented;
+                getCurrentVM = unimplemented;
+                getMaximumVM = unimplemented;
             }
+
+            // I know. Should be ulong. User with EB of memory can file a bug
+            long maxAllowedMem = IsX64() ? long.MaxValue : uint.MaxValue;
+
+            if ((long)getMaximumVM() != 0)
+            {
+                maxAllowedMem = (long)getMaximumVM();
+                Debug.Log("[GCMonitor] Maximum usable memory " + ConvertToGBString(maxAllowedMem) + " / " + ConvertToMBString(maxAllowedMem));
+            }
+
+            warnMem = (long)(maxAllowedMem * warnPercent);
+            alertMem = (long)(maxAllowedMem * alertPercent);
 
             watch = new Stopwatch();
             watch.Start();
@@ -278,7 +290,7 @@ namespace GCMonitor
             Thread threadGCCollector = new Thread(GCCollector);
             threadGCCollector.Start();
         }
-        
+
         internal void OnDestroy()
         {
             killThread = true;
@@ -319,7 +331,7 @@ namespace GCMonitor
                 dt -= 1.0f / updateRate;
             }
 
-            memory = (long)getCurrentRSS();
+            memory = (long)getCurrentVM();
             memoryString = ConvertToMBString(memory);
 
             lmb = lmb | Input.GetMouseButtonDown(0);
@@ -335,6 +347,12 @@ namespace GCMonitor
             if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(KeyCode.F))
             {
                 memoryGizmo = !memoryGizmo;
+            }
+
+            if (GameSettings.MODIFIER_KEY.GetKey() && Input.GetKeyDown(KeyCode.M))
+            {
+                Texture2D t = new Texture2D(5120, 5120, TextureFormat.ARGB32, false);
+                t.Apply();
             }
 
             if (!showUI)
@@ -396,7 +414,7 @@ namespace GCMonitor
             }
             else
             {
-                // To avoid problem with the thread updating 
+                // To avoid problem with the thread updating
                 // displayUpToSecond while we're in the while loop
                 int localdisplayUpToSecond = displayUpToSecond;
                 if (lastDisplayedSecond != localdisplayUpToSecond)
@@ -405,7 +423,7 @@ namespace GCMonitor
                     {
                         lastDisplayedSecond = (lastDisplayedSecond + 1) % width;
                         UpdateTexture(lastDisplayedSecond, localdisplayUpToSecond);
-                        
+
                     }
                     memoryTexture.Apply();
                 }
@@ -537,8 +555,8 @@ namespace GCMonitor
 
         public void memoryHistoryUpdate()
         {
-            
-            activeSecond = Mathf.FloorToInt(watch.ElapsedMilliseconds * 0.001f * timeScale) % width; 
+
+            activeSecond = Mathf.FloorToInt(watch.ElapsedMilliseconds * 0.001f * timeScale) % width;
 
             long mem = GC.GetTotalMemory(false);
 
@@ -550,7 +568,7 @@ namespace GCMonitor
 
                 // GC.MaxGeneration is always 0 on our current mono
                 // Sum all gen if it ever changes
-                int colCount = GC.CollectionCount(GC.MaxGeneration); 
+                int colCount = GC.CollectionCount(GC.MaxGeneration);
 
                 memoryHistory[previousActiveSecond].gc = colCount - lastColCount;
                 lastColCount = colCount;
@@ -563,7 +581,7 @@ namespace GCMonitor
             if (mem > memoryHistory[activeSecond].max)
                 memoryHistory[activeSecond].max = mem;
 
-            memoryHistory[activeSecond].rss = (long)getCurrentRSS();
+            memoryHistory[activeSecond].rss = (long)getCurrentVM();
         }
 
 
@@ -601,7 +619,7 @@ namespace GCMonitor
                 //    }
                 //}
 
-                // Since windows time make this sleep way more I could 
+                // Since windows time make this sleep way more I could
                 // remove it or use 0. Need testing on a slower CPU.
                 Thread.Sleep(5);
 
@@ -670,7 +688,7 @@ namespace GCMonitor
                         else
                             color = Color.black;
                     }
-                    
+
                     line[y] = color;
                 }
 
@@ -757,7 +775,7 @@ namespace GCMonitor
                 }
             }
             GUILayout.Space(30);
-            
+
             bool preveReal = realMemory;
             realMemory = GUILayout.Toggle(realMemory, "KSP process", GUILayout.ExpandWidth(false));
             if (preveReal != realMemory)
@@ -779,22 +797,22 @@ namespace GCMonitor
                 fullUpdate = true;
 
             OnlyUpdateWhenDisplayed = GUILayout.Toggle(OnlyUpdateWhenDisplayed, "Only Update When Display is visible", GUILayout.ExpandWidth(false));
-            
+
             GUILayout.EndHorizontal();
             GUILayout.BeginHorizontal();
 
-            GUILayout.Label("KSP: " + ConvertToKBString((long)getCurrentRSS()) + " / " + ConvertToKBString((long)getPeakRSS()), GUILayout.ExpandWidth(false));
-            
+            GUILayout.Label("KSP: " + ConvertToKBString((long)getCurrentVM()) + " / " + ConvertToKBString((long)getMaximumVM()), GUILayout.ExpandWidth(false));
+
             GUILayout.Space(20);
-            
+
             GUILayout.Label(
                 "Mono allocated:" + ConvertToMBString(Profiler.GetTotalAllocatedMemory())
                 + " min: " + ConvertToMBString(memoryHistory[activeSecond].min)
                 + " max: " + ConvertToMBString(memoryHistory[activeSecond].max)
                 + " GC : " + memoryHistory[previousActiveSecond].gc.ToString(), GUILayout.ExpandWidth(false));
-            
+
             GUILayout.Space(20);
-            
+
             GUILayout.Label("FPS: " + fps.ToString("0.0"), GUILayout.ExpandWidth(false));
 
             GUILayout.Space(20);
@@ -857,7 +875,7 @@ namespace GCMonitor
             if (GUILayout.Button("+", GUILayout.ExpandWidth(false)))
                 fpsSize++;
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginHorizontal();
             GUILayout.Label("X", GUILayout.Width(40));
             if (GUILayout.Button("-", GUILayout.ExpandWidth(false)))
@@ -875,7 +893,7 @@ namespace GCMonitor
             if (GUILayout.Button("+", GUILayout.ExpandWidth(false)))
                 fpsY = fpsY + 5;
             GUILayout.EndHorizontal();
-            
+
             GUILayout.EndVertical();
 
             fpsX = Mathf.Clamp(fpsX, 0, Screen.width);
@@ -885,7 +903,7 @@ namespace GCMonitor
 
         static String ConvertToGBString(long bytes)
         {
-            return ((bytes >> 20) / 1024f).ToString("00.0", spaceFormat);
+            return ((bytes >> 20) / 1024f).ToString("#,0.0", spaceFormat) + " GB";
         }
 
         static String ConvertToMBString(long bytes)
@@ -906,7 +924,7 @@ namespace GCMonitor
             {
                 GUI.Label(new Rect(r.x - strength, r.y + i, r.width, r.height), t, style);
                 GUI.Label(new Rect(r.x + strength, r.y + i, r.width, r.height), t, style);
-            } 
+            }
             for (int i = -strength + 1; i <= strength - 1; i++)
             {
                 GUI.Label(new Rect(r.x + i, r.y - strength, r.width, r.height), t, style);
@@ -921,6 +939,6 @@ namespace GCMonitor
         {
             MonoBehaviour.print("[GCMonitor] " + message.ToString());
         }
- 
+
     }
 }
